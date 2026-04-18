54f5960c72968e01e8bccd1a9cd261f865afa543
wyq2567 <69411205+wyq2567@users.noreply.github.com>
2026-04-18 14:03:35 +0800

    Add mybatis-autoGenerator module
    mybatisplus自动生成

---
+++ RuoYi-Cloud/mybatis-autoGenerator/src/main/java/com/ruoyi/Main.java
@@ -0,0 +1,37 @@
+package com.ruoyi;
+
+
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.nio.file.Paths;
+
+//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
+// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
+public class Main {
+    public static void main(String[] args) {
+        FastAutoGenerator.create("jdbc:jdbc
+                        , "root"
+                        , "root")
+                .globalConfig(builder -> builder
+                        .author("ruoyi")
+                        .outputDir(Paths.get(System.getProperty("user.dir")) + "/src/main/java")
+                        .commentDate("yyyy-MM-dd")
+                )
+                .packageConfig(builder -> builder
+                        .parent("com.ruoyi.generator")
+                        .entity("entity")
+                        .mapper("mapper")
+                        .service("service")
+                        .serviceImpl("service.impl")
+                        .xml("mapper.xml")
+                )
+                .strategyConfig(builder -> builder
+                        .addInclude("tb_xxx")
+                        .entityBuilder()
+                        .enableLombok()
+                )
+                .templateEngine(new FreemarkerTemplateEngine())
+                .execute();
+    }
+}
